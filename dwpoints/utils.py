f89c3f99f47af3cf1a5c98c6872b48fc8fe38efa
BGuzder-Williams.Local <bguzder-williams.local@j4n443d4xk.lan>
2023-02-02 15:11:46 -0800

    utils: accuracy

--- dwpoints/utils.py
+++ dwpoints/utils.py
@@ -42,3 +42,29 @@ class Timer(object):
         return self.end.strftime(self.fmt)
     def delta(self):
         return str(self.end-self.start)
+
+
+#
+# SCORING
+#
+def _equality_row(row,label_col,pred_cols):
+    eq_dict={ pcol: int(row[label_col]==row[pcol]) for pcol in pred_cols }
+    eq_dict['label']=row[label_col]
+    return eq_dict
+    
+    
+def get_acc_df(df,label_col,pred_cols):
+    eq=pd.DataFrame(df.apply(
+        _equality_row,
+        axis=1,
+        label_col=label_col,
+        pred_cols=pred_cols).tolist())
+    counts=eq.groupby(label_col).sum()
+    counts['total']=[df[df.label==l].shape[0] for l in counts.index]
+    accs=counts[pred_cols].divide(counts['total'],axis=0)
+    accs.rename(columns={ c:f'{c}_acc' for c in pred_cols},inplace=True)
+    counts.rename(columns={ c:f'{c}_count' for c in pred_cols},inplace=True)
+    return accs.join(counts)
+
+
+
